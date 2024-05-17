621e4900d5799e0869a447fd701d4f23c458c4ca
Groinkb <benjamin@mossu.ch>
2024-05-17 10:27:32 +0200

    add the rkeal  code to the algorythm

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,12 +1,26 @@
-#!/usr/bin/python3
+def canUnlockAll(boxes):
+    # Nombre total de boîtes
+    n = len(boxes)
+    
+    # Une liste pour suivre les boîtes déverrouillées
+    unlocked = [False] * n
+    unlocked[0] = True
+    
+    # Une pile pour gérer les boîtes à explorer (DFS)
+    stack = [0]
+    
+    while stack:
+        # Prendre une boîte de la pile
+        current_box = stack.pop()
+        
+        # Explorer les clés dans la boîte actuelle
+        for key in boxes[current_box]:
+            if key < n and not unlocked[key]:
+                # Si la clé correspond à une boîte valide et non déverrouillée
+                unlocked[key] = True
+                stack.append(key)
+    
+    # Vérifier si toutes les boîtes ont été déverrouillées
+    return all(unlocked)
 
-canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
-
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
