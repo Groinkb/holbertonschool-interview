e24022cc26fd4eebea002bbc297d18ee276fe3b7
Groinkb <benjamin@mossu.ch>
2024-05-27 11:42:13 +0200

    ADD MINIMUM OP

---
+++ minimum_operations/0-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Main file for testing
+BM
+"""
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
