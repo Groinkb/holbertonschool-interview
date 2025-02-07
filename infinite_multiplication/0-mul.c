5cc19f48e9af40ae2a888acb3251e71c608c700e
Groinkb <benjamin@mossu.ch>
2025-02-07 12:41:53 +0100

    betty style

--- infinite_multiplication/0-mul.c
+++ infinite_multiplication/0-mul.c
@@ -4,10 +4,18 @@
 #include <ctype.h>
 #include <string.h>
 
-/* Vérifie si une chaîne est composée uniquement de chiffres */
-int is_digit_str(char *str) {
-    while (*str) {
-        if (!isdigit(*str)) {
+/**
+ * is_digit_str - Vérifie si une chaîne est composée uniquement de chiffres
+ * @str: La chaîne à vérifier
+ *
+ * Return: 1 si tous les caractères sont des chiffres, sinon 0
+ */
+int is_digit_str(char *str)
+{
+    while (*str)
+    {
+        if (!isdigit(*str))
+        {
             return (0);
         }
         str++;
@@ -15,89 +23,20 @@ int is_digit_str(char *str) {
     return (1);
 }
 
-/* Fonction pour imprimer une chaîne de caractères représentant un grand nombre */
-void print_number(char *str) {
-    while (*str) {
+/**
+ * print_number - Imprime une chaîne de caractères représentant un grand nombre
+ * @str: La chaîne à imprimer
+ */
+void print_number(char *str)
+{
+    while (*str)
+    {
         _putchar(*str);
         str++;
     }
     _putchar('\n');
 }
 
-/* Multiplie deux grands nombres représentés sous forme de chaînes */
-char *mul(char *num1, char *num2) {
-    int len1 = strlen(num1);
-    int len2 = strlen(num2);
-    int len_result = len1 + len2;
-
-    /* Tableau pour stocker le résultat intermédiaire */
-    int *result = calloc(len_result, sizeof(int));
-    if (!result) {
-        return NULL;
-    }
-
-    /* Multiplication des chiffres de droite à gauche */
-    for (int i = len1 - 1; i >= 0; i--) {
-        for (int j = len2 - 1; j >= 0; j--) {
-            int mul = (num1[i] - '0') * (num2[j] - '0');
-            int p1 = i + j;
-            int p2 = i + j + 1;
-            int sum = mul + result[p2];
-            result[p1] += sum / 10;
-            result[p2] = sum % 10;
-        }
-    }
-
-    /* Convertir le tableau en chaîne de caractères */
-    char *result_str = malloc(len_result + 1);
-    if (!result_str) {
-        free(result);
-        return NULL;
-    }
-
-    int k = 0;
-    int i = 0;
-
-    /* Ignore les zéros en tête */
-    while (i < len_result && result[i] == 0) i++;
-
-    /* Conversion en chaîne de caractères */
-    while (i < len_result) {
-        result_str[k++] = result[i++] + '0';
-    }
-
-    /* Si le résultat est 0 */
-    if (k == 0) {
-        result_str[k++] = '0';
-    }
-
-    result_str[k] = '\0';
-
-    free(result);
-    return result_str;
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Error\n");
-        return (98);
-    }
-
-    if (!is_digit_str(argv[1]) || !is_digit_str(argv[2])) {
-        printf("Error\n");
-        return (98);
-    }
-
-    /* Appel à la fonction de multiplication */
-    char *result = mul(argv[1], argv[2]);
-
-    if (result) {
-        print_number(result);
-        free(result);
-    } else {
-        printf("Error\n");
-        return (98);
-    }
-
-    return (0);
-}
+/**
+ * mul - Multiplie deux grands nombres représentés sous forme de chaînes
+*/
