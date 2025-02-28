2156547a147863ff4a40fa65883723b5110393a3
Groinkb <benjamin@mossu.ch>
2025-02-28 13:05:58 +0100

    Implémentation de la fonction is_palindrome pour les listes chaînées
    - Ajout de l'algorithme de vérification de palindrome dans une liste chaînée
    - Utilisation de la technique du pointeur lent/rapide pour trouver le milieu
    - Implémentation d'une fonction de renversement de liste auxiliaire
    - Comparaison de la première et de la seconde moitié de la liste
    - Gestion des cas spéciaux (liste vide ou à un seul élément)

---
+++ linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,69 @@
+#include "lists.h"
+
+/**
+ * reverse_list - Reverses a linked list
+ * @head: Pointer to the head of the list
+ * Return: Pointer to the new head
+ */
+static listint_t *reverse_list(listint_t *head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = head;
+	listint_t *next = NULL;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	return (prev);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome
+ * @head: Double pointer to the head of the list
+ * Return: 1 if palindrome, 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *second_half, *prev_slow = *head;
+	int is_palindrome = 1;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	/* Find the middle using slow/fast pointer technique */
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		prev_slow = slow;
+		slow = slow->next;
+	}
+
+	/* If list has odd number of elements, skip the middle element */
+	if (fast != NULL)
+	{
+		slow = slow->next;
+	}
+
+	/* Reverse the second half */
+	second_half = reverse_list(slow);
+
+	/* Compare first half with reversed second half */
+	while (second_half != NULL)
+	{
+		if ((*head)->n != second_half->n)
+		{
+			is_palindrome = 0;
+			break;
+		}
+		*head = (*head)->next;
+		second_half = second_half->next;
+	}
+
+	return (is_palindrome);
+}
