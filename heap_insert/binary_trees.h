327277cd54463b589c8944a63154f4d2726272be
Groinkb <benjamin@mossu.ch>
2025-02-28 11:30:41 +0100

    Mise à jour de l'en-tête binary_trees.h pour l'insertion dans un tas binaire
    Ajout des prototypes de fonctions pour :
    - heap_insert() : insertion d'un élément dans un tas binaire maximal
    - binary_tree_size() : calcul de la taille de l'arbre binaire

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -24,6 +24,8 @@ typedef struct binary_tree_s heap_t;
 
 /* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+heap_t *heap_insert(heap_t **root, int value);
+size_t binary_tree_size(const binary_tree_t *tree);
 void binary_tree_print(const binary_tree_t *);
 
 #endif /* BINARY_TREES_H */
