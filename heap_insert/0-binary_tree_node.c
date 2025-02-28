a5625ea043738a0ac60164fb084345b58f6b1270
Groinkb <benjamin@mossu.ch>
2025-02-28 10:21:45 +0100

    Création du fichier de définition de nœud pour l'arbre binaire
    Ajout du fichier source 0-binary_tree_node.c avec la définition initiale de la structure de nœud pour l'implémentation du tas binaire. Inclusion des en-têtes nécessaires et déclaration de la structure Node avec ses attributs de base.

---
+++ heap_insert/0-binary_tree_node.c
@@ -0,0 +1,13 @@
+#include <include/binary_trees.h>
+#include <stdio.h>
+#include <stlib.h>
+
+
+//define a struct for a binary tree node
+typedef struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+} Node; 
+
+
