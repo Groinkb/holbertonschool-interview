632d703444afb0704286a26362da8b1a8ebf5ae5
Groinkb <benjamin@mossu.ch>
2025-03-27 16:00:18 +0100

    fix betty indentation

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -1,5 +1,4 @@
 #include "search.h"
-
 /**
  * linear_skip - searches for a value in a sorted skip list of integers
  * @list: pointer to the head of the skip list to search in
@@ -9,45 +8,39 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    skiplist_t *express, *current;
+skiplist_t *express, *current;
 
     /* Check if list is NULL */
-    if (list == NULL)
-        return (NULL);
-
-    express = list;
-    current = list;
-
+if (list == NULL)
+return (NULL);
+express = list;
+current = list;
     /* Traverse express lanes */
-    while (express->express != NULL && express->n < value)
-    {
-        current = express;
-        express = express->express;
-        printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
+while (express->express != NULL && express->n < value)
+{
+current = express;
+express = express->express;
+printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
     }
-
     /* If express lane ends or we found our search range */
-    if (express->express == NULL && express->n < value)
-    {
+if (express->express == NULL && express->n < value)
+{
         /* Save the start of our range for searching */
-        current = express;
+current = express;
         /* Find the last node of the list */
-        while (express->next != NULL)
-            express = express->next;
-    }
-
-    printf("Value found between indexes [%lu] and [%lu]\n",
-           current->index, express->index);
-
+while (express->next != NULL)
+express = express->next;
+}
+printf("Value found between indexes [%lu] and [%lu]\n",
+current->index, express->index);
     /* Linear search between the nodes */
-    while (current != NULL && current->index <= express->index)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
-        if (current->n == value)
-            return (current);
-        current = current->next;
-    }
-
+while (current != NULL && current->index <= express->index)
+{
+printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
+if (current->n == value)
+return (current);
+current = current->next;
+}
     /* Value not found */
-    return (NULL);
+return (NULL);
 }
--- linear_skip/search.h
+++ linear_skip/search.h
@@ -1,11 +1,9 @@
 #ifndef _SEARCH_H_
 #define _SEARCH_H_
-
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-
 /**
  * struct skiplist_s - Singly linked list with an express lane
  *
@@ -19,15 +17,13 @@
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+int n;
+size_t index;
+struct skiplist_s *next;
+struct skiplist_s *express;
 } skiplist_t;
-
 skiplist_t *create_skiplist(int *array, size_t size);
 void print_skiplist(const skiplist_t *list);
 void free_skiplist(skiplist_t *list);
 skiplist_t *linear_skip(skiplist_t *list, int value);
-
 #endif /* _SEARCH_H_ */
