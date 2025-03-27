647b0c853b91dddf0384470594c075cf65e2bc6f
Groinkb <benjamin@mossu.ch>
2025-03-27 15:40:23 +0100

    fix : to respect the betty style

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * create_node - Creates a new AVL tree node
@@ -10,18 +9,18 @@
  */
 avl_t *create_node(int n)
 {
-    avl_t *new_node;
+avl_t *new_node;
 
-    new_node = malloc(sizeof(avl_t));
-    if (new_node == NULL)
-        return (NULL);
+new_node = malloc(sizeof(avl_t));
+if (new_node == NULL)
+return (NULL);
 
-    new_node->n = n;
-    new_node->parent = NULL;
-    new_node->left = NULL;
-    new_node->right = NULL;
+new_node->n = n;
+new_node->parent = NULL;
+new_node->left = NULL;
+new_node->right = NULL;
 
-    return (new_node);
+return (new_node);
 }
 
 /**
@@ -35,34 +34,34 @@ avl_t *create_node(int n)
  */
 avl_t *build_avl(int *array, size_t start, size_t end, avl_t *parent)
 {
-    avl_t *root;
-    size_t mid;
+avl_t *root;
+size_t mid;
 
     /* Base case */
-    if (start > end || array == NULL)
-        return (NULL);
+if (start > end || array == NULL)
+return (NULL);
 
     /* Get the middle element and make it root */
-    mid = (start + end) / 2;
+mid = (start + end) / 2;
 
     /* Create the node */
-    root = create_node(array[mid]);
-    if (root == NULL)
-        return (NULL);
+root = create_node(array[mid]);
+if (root == NULL)
+return (NULL);
 
     /* Set parent */
-    root->parent = parent;
+root->parent = parent;
 
     /* Recursively build left subtree */
-    if (mid > start)
-        root->left = build_avl(array, start, mid - 1, root);
-    else
-        root->left = NULL;
+if (mid > start)
+root->left = build_avl(array, start, mid - 1, root);
+else
+root->left = NULL;
 
     /* Recursively build right subtree */
-    root->right = build_avl(array, mid + 1, end, root);
+root->right = build_avl(array, mid + 1, end, root);
 
-    return (root);
+return (root);
 }
 
 /**
@@ -74,8 +73,8 @@ avl_t *build_avl(int *array, size_t start, size_t end, avl_t *parent)
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    if (array == NULL || size == 0)
-        return (NULL);
+if (array == NULL || size == 0)
+return (NULL);
 
-    return (build_avl(array, 0, size - 1, NULL));
+return (build_avl(array, 0, size - 1, NULL));
 }
