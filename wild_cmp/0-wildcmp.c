ec8d3d96b9f4e2023067f77c969e931c2fd8f3df
Groinkb <benjamin@mossu.ch>
2025-03-28 12:53:36 +0100

    fix : indentation ?

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -9,28 +9,28 @@
 int wildcmp(char *s1, char *s2)
 {
     /* If both strings are empty, they are identical */
-    if (*s1 == '\0' && *s2 == '\0')
-        return (1);
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
 
     /* If s2 has a '*' */
-    if (*s2 == '*')
-    {
+if (*s2 == '*')
+{
         /* If the next character in s2 is also '*', skip it */
-        if (*(s2 + 1) == '*')
-            return (wildcmp(s1, s2 + 1));
+if (*(s2 + 1) == '*')
+return (wildcmp(s1, s2 + 1));
 
         /* Skip characters in s1 until a match is found for the rest of s2 */
-        if (*s1 == '\0')
-            return (wildcmp(s1, s2 + 1));
+if (*s1 == '\0')
+return (wildcmp(s1, s2 + 1));
 
         /* Two possibilities: either we use the * or we don't */
-        return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-    }
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+}
 
     /* If current characters match, continue with next characters */
-    if (*s1 == *s2)
-        return (wildcmp(s1 + 1, s2 + 1));
+if (*s1 == *s2)
+return (wildcmp(s1 + 1, s2 + 1));
 
     /* If we reach here, the strings are not identical */
-    return (0);
+return (0);
 }
