8d0298b4a0d069b1e822b6b9f1a0955c78646b5b
Groinkb <benjamin@mossu.ch>
2025-04-06 12:23:46 +0200

    add new line at the end

--- regex/regex.c
+++ regex/regex.c
@@ -10,36 +10,36 @@
 static int match_helper(char const *str, char const *pattern)
 {
   /* If pattern is empty, string must also be empty */
-  if (*pattern == '\0')
-    return (*str == '\0');
+if (*pattern == '\0')
+return (*str == '\0');
 
-  /* Look ahead to check if the next character is '*' */
-  int is_star = *(pattern + 1) == '*';
+  /* Look ahead to check  the next character is '*' */
+int is_star = *(pattern + 1) == '*';
 
   /* If next char is not '*', standard matching for current character */
-  if (!is_star)
-  {
+if (!is_star)
+{
     /* Current char must match pattern or pattern is '.' and str not empty */
-    if (*str != '\0' && (*pattern == '.' || *pattern == *str))
-      return match_helper(str + 1, pattern + 1);
-    return 0;
-  }
-  else /* Next char is '*' */
-  {
+if (*str != '\0' && (*pattern == '.' || *pattern == *str))
+return match_helper(str + 1, pattern + 1);
+return 0;
+}
+else /* Next char is '*' */
+{
     /* Try matching zero characters (skip the pattern with '*') */
-    if (match_helper(str, pattern + 2))
-      return 1;
+if (match_helper(str, pattern + 2))
+return 1;
 
     /* Try matching one or more characters */
-    while (*str != '\0' && (*pattern == '.' || *pattern == *str))
-    {
+while (*str != '\0' && (*pattern == '.' || *pattern == *str))
+{
       /* Match one character and try again with rest of string */
-      str++;
-      if (match_helper(str, pattern + 2))
-        return 1;
-    }
-    return 0;
-  }
+str++;
+if (match_helper(str, pattern + 2))
+return 1;
+}
+return 0;
+}
 }
 
 /**
@@ -51,8 +51,8 @@ static int match_helper(char const *str, char const *pattern)
  */
 int regex_match(char const *str, char const *pattern)
 {
-  if (!str || !pattern)
-    return 0;
+if (!str || !pattern)
+return 0;
 
-  return match_helper(str, pattern);
+return match_helper(str, pattern);
 }
